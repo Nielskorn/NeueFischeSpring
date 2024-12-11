99e21dbb11e57de11bb2105e45577b3e8f3b4ca9
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-12-11 11:53:51 +0100

    added Message and added methodes in controller for created read and Deleted

--- Neuefische_Spring/src/main/java/com/example/Neuefische_Spring/ApiController.java
+++ Neuefische_Spring/src/main/java/com/example/Neuefische_Spring/ApiController.java
@@ -1,17 +1,51 @@
 package com.example.Neuefische_Spring;
 
+import org.apache.logging.log4j.message.Message;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping(path ="/api/hello")
+@RequestMapping(path ="/api")
 public class ApiController {
-    @GetMapping
-    public String HelloWorld() {
-        return "Hello, World!";
+    List<Massage>messages=new ArrayList<>();
+
+
+    @GetMapping("/hello/{name}")
+
+    public String HelloWorld(@PathVariable String name) {
+        return "Hello,"+name+"!";
     }
+
+    @PostMapping("/messages")
+    public String addMessage(@RequestBody Massage message) {
+        messages.add(message);
+        return "Message added!";
+    }
+    @GetMapping("/messages")
+    public List<String> getMessages() {
+        List<String> list=new ArrayList<>();
+        for (Massage m : messages) {
+            list.add(m.toString());
+        }
+        return list;
+    }
+    @DeleteMapping("/messages/{id}")
+    public String deleteMessage(@PathVariable String id) {
+        for (Massage m : messages) {
+            if (m.getId().equals(id)) {
+                messages.remove(m);
+                return "Message deleted!";
+            }
+        }
+        return "Message not found!";
+    }
+
+
+
+
 }
